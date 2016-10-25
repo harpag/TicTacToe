c872803f195a3d98b361377e8b1862ff81c53c77
Harpa Guðjónsdóttir <harpa09@i-1217c9a8.cs1004cloud.internal>
2016-10-25 16:29:28 +0000

    Added compile,deploy,package,run and unit_test scripts

--- src/main/java/is/ru/tictactoe/World.java
+++ src/main/java/is/ru/tictactoe/World.java
@@ -1,4 +1,4 @@
-package is.ru.tictactoe
+package is.ru.tictactoe;
 
 public class World {
 
--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class WorldTest {
+public class TicTacToeTest {
 
     @Test
     public void greetResultsInHello() {
