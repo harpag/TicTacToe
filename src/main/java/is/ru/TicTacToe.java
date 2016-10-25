fb6c2524276c75efb827a2839232863ad7afa92b
Harpa Guðjónsdóttir <harpa09@i-1217c9a8.cs1004cloud.internal>
2016-10-25 15:43:57 +0000

    Gradle project initialized

---
+++ src/main/java/is/ru/TicTacToe.java
@@ -0,0 +1,9 @@
+package is.ru.tictactoe;
+
+
+public class TicTacToe {
+    public static void main(String[] args) {
+    	World world = new World();    
+    	System.out.println(world.greet());
+    }
+}
---
+++ src/main/java/is/ru/World.java
@@ -0,0 +1,10 @@
+package is.ru.tictactoe
+
+public class World {
+
+    private String greeting = "Hello world!";
+
+    public String greet() {
+        return greeting;
+    }
+}
